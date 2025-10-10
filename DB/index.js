6447bc276f86acb7fa418f887cbde9f0acafddc4
Harjinder Singh <harjindersingh@Harjinders-MacBook-Pro-3.local>
2025-10-11 01:49:41 +0530

    db updated

--- DB/index.js
+++ DB/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const URL = process.env.DATABASE;
 
-mongoose.connect("mongodb+srv://Kaka5611:Kaka5611@cluster0.tqjcw.mongodb.net/?retryWrites=true&w=majority",{
+mongoose.connect(URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
